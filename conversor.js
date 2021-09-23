7e4e6cc8bf3413fcac9f1971a8ce70a0201a13fd
Daniel Morales <danisan98@hotmail.es>
2021-09-23 17:58:49 -0500

    Initial Commit

---
+++ conversor.js
@@ -0,0 +1,17 @@
+let resul ="";
+let constanteConversion = 1.8;
+let constanteSuma = 32;
+
+let CtoF = (grados) => (grados*constanteConversion)+constanteSuma;
+
+
+document.getElementById('btnCelsToFah').get
+
+function celsiusToFahrenheit(grados){
+    document.getElementById('resulFahrenheitBox').style.visibility = "visible"
+    document.getElementById('resulFahrenheit').innerHTML = CtoF(grados);
+}
+
+
+
+ 
